928e686a9fedacd288465550a97389f665ccbf24
apanacode <145982090+apanacode@users.noreply.github.com>
2023-12-05 23:26:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let modebtn=document.querySelector("#button");
+let body= document.querySelector("body");
+
+let currentmode="light";
+modebtn.addEventListener("click", () => {
+if(currentmode === "light")
+{
+   currentmode="dark";
+ 
+  body.classList.add("dark");
+  body.classList.remove("light");
+}
+else{
+   currentmode="light";
+   
+   body.classList.add("light");
+   body.classList.remove("dark");
+}
+console.log(currentmode);
+});
+
